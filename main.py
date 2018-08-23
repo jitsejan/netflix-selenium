f49d064a20547b06cc2b43e83c41b16515ab18df
Jitse-Jan <jitsejan@gmail.com>
2018-08-24 00:38:09 +0100

    Added set faves function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import re
 import pandas as pd
+import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
@@ -11,35 +12,30 @@ PASSWORD = os.environ.get("NETFLIX_PASS")
 URL = "https://www.netflix.com/browse/my-list"
 REGEX = r"https://www.netflix.com/watch/(.*?)\?tctx"
 
-def main():
-    browser = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME)
-    print("Running Netflix Selenium")
-    browser.get(URL)
+def login(browser):
     print(browser.current_url)
     try:
-        print("Try")
         username_field = browser.find_element_by_id('id_userLoginId')
-        print(username_field)
         username_field.send_keys(USERNAME)
         password_field = browser.find_element_by_id("id_password")
-        print(password_field)
         password_field.send_keys(PASSWORD)
     except:
-        print("Except")
         username_field = browser.find_element_by_id('email')
-        print(username_field)
         username_field.send_keys(USERNAME)
         password_field = browser.find_element_by_id("password")
-        print(password_field)
         password_field.send_keys(PASSWORD)
     login_button = browser.find_element_by_class_name('login-button')
     login_button.click()
+
+def profileselect(browser):
     print(browser.current_url)
     try:
         profile_button = browser.find_element_by_class_name('profile-icon')
         profile_button.click()
     except:
         pass
+
+def get_favorites(browser):
     items = []
     print(browser.find_elements_by_class_name('ptrack-content'))
     for elem in browser.find_elements_by_class_name('ptrack-content'):
@@ -49,9 +45,33 @@ def main():
         }
         item['id'] = re.search(REGEX, item['viewlink']).group(1)
         item['infolink'] = f"https://www.netflix.com/title/{item['id']}"
-        print(item)
         items.append(item)
-    pd.DataFrame(items).to_csv('/app/output/items.csv', index=False)
+    pd.DataFrame(items).to_csv('output/items.csv', index=False)
+
+def set_favorites(browser):
+    df = pd.read_csv('output/items.csv')
+    for elem in df['infolink']:
+        print(f"Adding {elem}")
+        try:
+            browser.get(elem)
+            list_button = browser.find_elements_by_class_name('nf-icon-button')[-1]
+            time.sleep(1)
+            list_button.click()
+        except:
+            print("FAILED")
+
+def main():
+    # browser = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME)
+    browser = webdriver.Chrome()
+    print("Running Netflix Selenium")
+    browser.get(URL)
+    login(browser)
+    profileselect(browser)
+    
+    set_favorites(browser)
+    
+    # get_favorites(browser)
+    
     browser.quit()
     
 if __name__ == "__main__":
