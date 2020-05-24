88a2e5776cbb67f3805937262b6247784e7b8308
nathan <nathan@hotmail.com>
2020-05-24 11:47:19 +0100

    change to headless, some parameters for account, some checking of ENV variables, update for multiple account usage

--- main.py
+++ main.py
@@ -3,16 +3,35 @@ import os
 import re
 import pandas as pd
 import time
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from argparse import ArgumentParser
 
-USERNAME = os.environ.get("NETFLIX_USER")
-PASSWORD = os.environ.get("NETFLIX_PASS")
+parser = ArgumentParser(description='Get or set "My List" from Netflix account provided to script')
+parser.add_argument('--action',help='provide "set" to update Netflix My List from csv or "get" to download data to csv')
+parser.add_argument('--account', help='Netflix account to be used with script')
+
+args = parser.parse_args()
+
+ACTION=args.action
+
+print(os.environ)
+if "NETFLIX_USER" and "NETFLIX_PASS" not in os.environ:
+    print("Please set environment variables for NETFLIX_USER AND NETFLIX_PASS")
+    sys.exit(0)
+
+USERNAME = os.environ["NETFLIX_USER"]
+PASSWORD = os.environ["NETFLIX_PASS"]
+ACCOUNT = args.account
 URL = "https://www.netflix.com/browse/my-list"
 REGEX = r"https://www.netflix.com/watch/(.*?)\?tctx"
 
+
 def login(browser):
+    print("logging in...")
     print(browser.current_url)
     try:
         username_field = browser.find_element_by_id('id_userLoginId')
@@ -27,15 +46,21 @@ def login(browser):
     login_button = browser.find_element_by_class_name('login-button')
     login_button.click()
 
-def profileselect(browser):
+
+def profileselect(browser, account):
+    print("profile select")
     print(browser.current_url)
     try:
-        profile_button = browser.find_element_by_class_name('profile-icon')
+        time.sleep(2)
+        profile_button = browser.find_element_by_xpath(f"//span[@class='profile-name' and contains(text(),'{account}')]")
         profile_button.click()
-    except:
+    except Exception as e:
+        print(e)
         pass
 
+
 def get_favorites(browser):
+    print("get favourites..")
     items = []
     print(browser.find_elements_by_class_name('ptrack-content'))
     for elem in browser.find_elements_by_class_name('ptrack-content'):
@@ -48,7 +73,9 @@ def get_favorites(browser):
         items.append(item)
     pd.DataFrame(items).to_csv('output/items.csv', index=False)
 
+
 def set_favorites(browser):
+    print("setting favourites")
     df = pd.read_csv('output/items.csv')
     for elem in df['infolink']:
         print(f"Adding {elem}")
@@ -57,22 +84,27 @@ def set_favorites(browser):
             list_button = browser.find_elements_by_class_name('nf-icon-button')[-1]
             time.sleep(1)
             list_button.click()
-        except:
-            print("FAILED")
+        except Exception as e:
+            print("Failed", e)
+
 
 def main():
     # browser = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME)
-    browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    browser = webdriver.Chrome("/path/to/your/chromedriver", options=chrome_options)
     print("Running Netflix Selenium")
     browser.get(URL)
     login(browser)
-    profileselect(browser)
-    
-    set_favorites(browser)
-    
-    # get_favorites(browser)
-    
+    profileselect(browser, ACCOUNT)
+
+    if ACTION == "set":
+        set_favorites(browser)
+    else:
+        get_favorites(browser)
+
     browser.quit()
-    
+
+
 if __name__ == "__main__":
     main()
